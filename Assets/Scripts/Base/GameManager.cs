fac7d3ea1d2dad22aee53645391d19d7267e824a
mirris2468 <179534388+Mirris2468@users.noreply.github.com>
2025-06-24 18:47:21 -0300

    Parcial-2 Final V.

--- Assets/Scripts/Base/GameManager.cs
+++ Assets/Scripts/Base/GameManager.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
-
+//Script del GameMananger que permite aparecer a los enemigos en sus respectivos spawns
 public class GameManager : MonoBehaviour
 {
-    public GameObject Enemy1;
-    public GameObject Enemy2;
-    public Transform[] spawnPoints;
+    public GameObject Enemy1;//Se coloca en el inspector el PreFab del enemigo 1
+    public GameObject Enemy2;//Se coloca en el inspector el PreFab del enemigo 2
+    public Transform[] spawnPoints;//Se colocan los Puntos del spawn en el inspector
 
-    void Start()
+    void Start()//Al empezar la escena el juego escoje al azar un enemigo y lo instancia en uno de los spawn (Esto lo hace por cada spawn (2 veces en este caso))
     {
         foreach (Transform spawn in spawnPoints)
         {
--- Assets/Scripts/Base/Object.cs
+++ Assets/Scripts/Base/Object.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 //Script de objetos donde se puede colocar un nombre personalizado para el objeto que utiliza este script
 public abstract class Object : MonoBehaviour, IInteractable
 {
-    public string objName;
+    public string objName; //Se le puede colocar un nombre en el inspector al objeto
     public abstract void Interact();
 
     protected virtual void Awake()
