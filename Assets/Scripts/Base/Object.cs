9542a4583febffcef18d9a03f73983da9eed7880
Mirris2468 <mirris2468@gmail.com>
2025-06-24 11:27:21 -0300

    Parcial-2
    Mini update

--- Assets/Scripts/Base/Object.cs
+++ Assets/Scripts/Base/Object.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+//Script de objetos donde se puede colocar un nombre personalizado para el objeto que utiliza este script
 public abstract class Object : MonoBehaviour, IInteractable
 {
     public string objName;
